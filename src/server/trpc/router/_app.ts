ef46d26700b901d607bc39cddc5c91c56c957543
Caio Roberto Hach <darkcaiobits@gmail.com>
2023-01-23 15:08:51 -0300

    list and search recipes implemented

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { apiRouter } from "./api";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  api: apiRouter,
 });
 
 // export type definition of API
